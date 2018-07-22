78d50924adccf529b0ad3d1a3c07dddefb8bad58
Craig Zeise <craig.zeise@gmail.com>
2018-07-22 13:48:38 -0500

    fix: Fix inputWidth typo

--- designs.js
+++ designs.js
@@ -1,10 +1,16 @@
-// Select color input
-// Select size input
+/* eslint-env browser */
 
-// When size is submitted by the user, call makeGrid()
+const sizeInput = document.getElementById('sizePicker');
+const table = document.getElementById('pixelCanvas');
 
 function makeGrid() {
 
 // Your code goes here!
 
 }
+
+// When size is submitted by the user, call makeGrid()
+sizeInput.addEventListener('submit', (event) => {
+  event.preventDefault();
+  makeGrid();
+});
