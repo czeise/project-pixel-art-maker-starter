5bc14cc3654b05bd3e9e377a807d22a59a61748b
Craig Zeise <craig.zeise@gmail.com>
2018-07-22 13:57:45 -0500

    feat: Build grid when grid size submitted

--- designs.js
+++ designs.js
@@ -4,9 +4,20 @@ const sizeInput = document.getElementById('sizePicker');
 const table = document.getElementById('pixelCanvas');
 
 function makeGrid() {
+  const height = document.getElementById('inputHeight').value;
+  const width = document.getElementById('inputWidth').value;
 
-// Your code goes here!
+  // Clear table if any rows currently exist
+  table.querySelectorAll('tr').forEach((row) => {
+    row.remove();
+  });
 
+  for (let row = 0; row < height; row += 1) {
+    table.appendChild(document.createElement('tr'));
+    for (let column = 0; column < width; column += 1) {
+      table.lastChild.appendChild(document.createElement('td'));
+    }
+  }
 }
 
 // When size is submitted by the user, call makeGrid()
